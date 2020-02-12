3578b5d2460033ced03c638ca1599948aee576ac
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-02-13 04:21:52 +0800

    userscript init.

---
+++ kater tools.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Kater Tools
+// @namespace    -
+// @version      0.1.0
+// @description  As title
+// @author       LianSheng
+// @include      https://kater.me*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
